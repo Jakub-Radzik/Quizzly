e4cbd85e8c092d978bd1ee30cac2b710f51b7662
Kamil Herbetko <herbetko.kamil@gmail.com>
2024-11-24 13:32:36 +0100

    Removing legacy quiz generation method.

--- next-app/src/app/api/quiz/generate/route.ts
+++ next-app/src/app/api/quiz/generate/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { ChatOpenAI } from "@langchain/openai";
-import { HumanMessage } from "@langchain/core/messages";
-
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 
 import saveQuiz from "./saveToDb";
 import saveToS3 from "./saveToS3";
@@ -60,81 +56,6 @@ export async function POST(req: NextRequest) {
 
     const texts = selectedDocuments.map((doc) => doc.pageContent);
     console.log(texts);
-
-    // Generate the quiz
-    // ----------------------------------------------------------------------------
-    /* const prompt = */
-    /*   'given the text which is a summar of the document, generate a quiz based on the text. Return json only that contains a quiz object with fields: name, description, and questions. The questions is an array of object with fields: questionText, answers. The answers is an array of objects with fields: answerText, isCorrect.' */
-    /**/
-    /* if (!process.env.OPENAI_API_KEY) { */
-    /*   return NextResponse.json( */
-    /*     { error: 'OpenAI API key not provided' }, */
-    /*     { status: 500 }, */
-    /*   ) */
-    /* } */
-    /* console.log('have openAI key') */
-    /**/
-    /* const model = new ChatOpenAI({ */
-    /*   openAIApiKey: process.env.OPENAI_API_KEY, */
-    /*   modelName: 'gpt-4-1106-preview', */
-    /* }) */
-    /**/
-    /* const parser = new JsonOutputFunctionsParser() */
-    /* const extractionFunctionSchema = { */
-    /*   name: 'extractor', */
-    /*   description: 'Extracts fields from output', */
-    /*   parameters: { */
-    /*     type: 'object', */
-    /*     properties: { */
-    /*       quiz: { */
-    /*         type: 'object', */
-    /*         properties: { */
-    /*           name: { type: 'string' }, */
-    /*           description: { type: 'string' }, */
-    /*           questions: { */
-    /*             type: 'array', */
-    /*             items: { */
-    /*               type: 'object', */
-    /*               properties: { */
-    /*                 questionText: { type: 'string' }, */
-    /*                 answers: { */
-    /*                   type: 'array', */
-    /*                   items: { */
-    /*                     type: 'object', */
-    /*                     properties: { */
-    /*                       answerText: { type: 'string' }, */
-    /*                       isCorrect: { type: 'boolean' }, */
-    /*                     }, */
-    /*                   }, */
-    /*                 }, */
-    /*               }, */
-    /*             }, */
-    /*           }, */
-    /*         }, */
-    /*       }, */
-    /*     }, */
-    /*   }, */
-    /* } */
-    /**/
-    /* const runnable = model */
-    /*   .bind({ */
-    /*     functions: [extractionFunctionSchema], */
-    /*     function_call: { name: 'extractor' }, */
-    /*   }) */
-    /*   .pipe(parser) */
-    /* console.log('bind and pipe') */
-    /**/
-    /* const message = new HumanMessage({ */
-    /*   content: [ */
-    /*     { */
-    /*       type: 'text', */
-    /*       text: prompt + '\n' + texts.join('\n'), */
-    /*     }, */
-    /*   ], */
-    /* }) */
-    /**/
-    /* const result = (await runnable.invoke([message])) as { quiz: Quiz } */
-    /* console.log(result) */
 
     console.log("before lambda");
     // Generate the quiz using AWS Lambda
