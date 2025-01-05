3bc2ab6c10d62b4424e1ceae8593146d91cfa9e3
Pitru <33040045+Pitru@users.noreply.github.com>
2025-01-05 23:17:37 +0100

    Add moodle export

--- src/app/quiz/StartQuiz.tsx
+++ src/app/quiz/StartQuiz.tsx
@@ -1,16 +1,24 @@
+import { exportMoodle } from "@/export_moodle/export";
+
+import { questionAnswers, quizzes, questions as DbQuestions} from "@/db/schema";
+import { InferSelectModel } from "drizzle-orm/table";
+
+type Answer = InferSelectModel<typeof questionAnswers>;
+type Question = InferSelectModel<typeof DbQuestions> & { answers: Answer[] };
+type Quiz = InferSelectModel<typeof quizzes> & { questions: Question[] };
+
 type Props = {
-  name?: string | null;
-  description?: string | null;
-  quizLength: number;
+  quizData: Quiz;
   onStart: () => void;
 };
 
 export const StartQuiz = ({
-  name,
-  description,
-  quizLength,
+  quizData,
   onStart,
 }: Props) => {
+  const name = quizData.name
+  const description = quizData.description
+  const qustionNumber = quizData.questions.length
   return (
     <div className="max-w-lg h-[50vh] mx-auto my-10 p-6 rounded-lg shadow-md flex flex-col justify-between">
       <h1 className="text-4xl font-extrabold text-center text-indigo-400">
@@ -20,7 +28,7 @@ export const StartQuiz = ({
       <div className="mt-6 text-center">
         <h2 className="text-2xl font-semibold text-gray-500">{name}</h2>
         <p className="mt-4 text-gray-400">{description}</p>
-        <h3 className="text-xl mt-4 text-gray-200">Questions: {quizLength}</h3>
+        <h3 className="text-xl mt-4 text-gray-200">Questions: {qustionNumber}</h3>
       </div>
 
       <div>
@@ -31,7 +39,9 @@ export const StartQuiz = ({
           Start Quiz
         </button>
         <button
-          onClick={() => {}}
+          onClick={async() => {
+            await exportMoodle(quizData)
+          }}
           className="mt-2 w-full py-3 text-white text-lg font-bold rounded-lg 
           hover:bg-gray-800 border-2 hover:border-gray-200 transition duration-300"
         >
