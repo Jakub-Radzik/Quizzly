31244c814868400326aca743d1b2f5d18576754a
Jakub Radzik <jradzik4@gmail.com>
2025-01-08 17:05:24 +0100

    hotfix

--- src/export_moodle/export.ts
+++ src/export_moodle/export.ts
@@ -3,123 +3,123 @@ import { quizzes } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 const exampleQuiz = {
-    id: 1,
-    name: "exampleQuiz",
-    description: "exampleDescription",
-    userId: "123456789",
-    sourceDocumentId: "123456789",
-    sourceDocumentAlias: "exampleAlias",
-    questions: [
+  id: 1,
+  name: "exampleQuiz",
+  description: "exampleDescription",
+  userId: "123456789",
+  sourceDocumentId: "123456789",
+  sourceDocumentAlias: "exampleAlias",
+  questions: [
+    {
+      id: 1,
+      quizId: 1,
+      questionText: "Co było pierwsze?",
+      answers: [
         {
-            id: 1,
-            quizId: 1,
-            questionText: "Co było pierwsze?",
-            answers: [
-                {
-                    id: 1,
-                    questionId: 1,
-                    answerText: "Kura",
-                    isCorrect: false,
-                },
-                {
-                    id: 2,
-                    questionId: 1,
-                    answerText: "Jajko",
-                    isCorrect: true,
-                },
-            ]
+          id: 1,
+          questionId: 1,
+          answerText: "Kura",
+          isCorrect: false,
         },
         {
-            id: 2,
-            quizId: 1,
-            questionText: "Co jest lepsze?",
-            answers: [
-                {
-                    id: 3,
-                    questionId: 2,
-                    answerText: "Pizza",
-                    isCorrect: true,
-                },
-                {
-                    id: 4,
-                    questionId: 2,
-                    answerText: "Hot Dog",
-                    isCorrect: true,
-                },
-                {
-                    id: 5,
-                    questionId: 2,
-                    answerText: "Hamburger",
-                    isCorrect: true,
-                },
-            ]
-        }
-    ]
+          id: 2,
+          questionId: 1,
+          answerText: "Jajko",
+          isCorrect: true,
+        },
+      ],
+    },
+    {
+      id: 2,
+      quizId: 1,
+      questionText: "Co jest lepsze?",
+      answers: [
+        {
+          id: 3,
+          questionId: 2,
+          answerText: "Pizza",
+          isCorrect: true,
+        },
+        {
+          id: 4,
+          questionId: 2,
+          answerText: "Hot Dog",
+          isCorrect: true,
+        },
+        {
+          id: 5,
+          questionId: 2,
+          answerText: "Hamburger",
+          isCorrect: true,
+        },
+      ],
+    },
+  ],
+};
+
+export async function exportMoodle(quizData: unknown) {
+  // const quiz = await db.query.quizzes.findFirst({
+  //     where: eq(quizzes.id, quizId),
+  //     with: {
+  //         questions: {
+  //             with: {
+  //                 answers: true,
+  //             },
+  //         },
+  //     },
+  // });
+  // @ts-ignore
+  if (!quizData || quizData.questions.length === 0) {
+    throw Error("Empty quiz");
+  }
+  const xmlContent = prepareXML(quizData);
+  const blob = new Blob([xmlContent], { type: "application/xml" });
+
+  // Create a downloadable link
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  // @ts-ignore
+  a.download = `${quizData.id}_moodle.xml`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
 
-type Quiz = InferSelectModel<typeof quizzes> & { questions: Question[] };
+// @ts-ignore
+function prepareXML(quiz) {
+  var questionsXML = "";
+  for (let i = 0; i < quiz.questions.length; i++) {
+    const question = quiz.questions[i];
+    const questionName = question.questionText;
+    const questionText = question.questionText;
 
-export async function exportMoodle(quizData: Quiz) {
-    // const quiz = await db.query.quizzes.findFirst({
-    //     where: eq(quizzes.id, quizId),
-    //     with: {
-    //         questions: {
-    //             with: {
-    //                 answers: true,
-    //             },
-    //         },
-    //     },
-    // });
-
-    if (!quizData || quizData.questions.length === 0) {
-        throw Error("Empty quiz")
+    var questionCorrectSum = 0;
+    for (let j = 0; j < question.answers.length; j++) {
+      if (question.answers[j].isCorrect) {
+        questionCorrectSum += 1;
+      }
     }
-    const xmlContent = prepareXML(quizData)
-    const blob = new Blob([xmlContent], { type: 'application/xml' })
-
-    // Create a downloadable link
-    const url = URL.createObjectURL(blob)
-    const a = document.createElement('a')
-    a.href = url
-    a.download = `${quizData.id}_moodle.xml`
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
-}
-
-function prepareXML(quiz){
-    var questionsXML = ""
-    for (let i = 0; i < quiz.questions.length; i++) {
-        const question = quiz.questions[i]
-        const questionName = question.questionText
-        const questionText = question.questionText
-
-        var questionCorrectSum = 0
-        for (let j = 0; j < question.answers.length; j++) {
-            if (question.answers[j].isCorrect){
-                questionCorrectSum += 1
-            }
-        }
-        if (questionCorrectSum == 0){
-            throw Error("Question has now correct answers")
-        }
-        const questionFraction = 100/questionCorrectSum
-        const questionSingle = (questionCorrectSum==1) ? "true" : "false"
-        var answersXML = ""
-        for (let j = 0; j < question.answers.length; j++) {
-            const answer = question.answers[j]
-            const answerText = answer.answerText
-            const isAnswerCorrect = answer.isCorrect
-            const answerFraction = (isAnswerCorrect) ? questionFraction : 0 
-            const answerFeedbackText = (isAnswerCorrect) ? "Correct!" : "Wrong!"
-            const answerXML = `
+    if (questionCorrectSum == 0) {
+      throw Error("Question has now correct answers");
+    }
+    const questionFraction = 100 / questionCorrectSum;
+    const questionSingle = questionCorrectSum == 1 ? "true" : "false";
+    var answersXML = "";
+    for (let j = 0; j < question.answers.length; j++) {
+      const answer = question.answers[j];
+      const answerText = answer.answerText;
+      const isAnswerCorrect = answer.isCorrect;
+      const answerFraction = isAnswerCorrect ? questionFraction : 0;
+      const answerFeedbackText = isAnswerCorrect ? "Correct!" : "Wrong!";
+      const answerXML = `
         <answer fraction="${answerFraction}">
             <text>${answerText}</text>
             <feedback><text>${answerFeedbackText}</text></feedback>
-        </answer>`
-            answersXML += answerXML
-        }
-        const questionXML = `
+        </answer>`;
+      answersXML += answerXML;
+    }
+    const questionXML = `
     <question type="multichoice">
         <name>
             <text>${questionName}</text>
@@ -129,12 +129,11 @@ function prepareXML(quiz){
         </questiontext>${answersXML}
         <shuffleanswers>1</shuffleanswers>
         <single>${questionSingle}</single>
-    </question>`
-        questionsXML += questionXML    
-    }
-    const output = 
-`<?xml version="1.0" ?> 
+    </question>`;
+    questionsXML += questionXML;
+  }
+  const output = `<?xml version="1.0" ?> 
 <quiz>${questionsXML}
-</quiz>`
-    return output
+</quiz>`;
+  return output;
 }
